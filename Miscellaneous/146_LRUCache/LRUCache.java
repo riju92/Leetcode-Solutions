264507d7c687272982f7dc0cb3e6c26dd3dbf3cf
Riju <59536881+riju92@users.noreply.github.com>
2020-05-15 15:05:33 -0400

    146_using dlinkedlist

--- Miscellaneous/146_LRUCache/LRUCache.java
+++ Miscellaneous/146_LRUCache/LRUCache.java
@@ -21,7 +21,7 @@ Could you do both operations in O(1) time complexity?
 
 Example:
 
-LRUCache cache = new LRUCache( 2 /* capacity */ );
+LRUCache cache = new LRUCache( 2 /* capacity */ /*);
 
 cache.put(1, 1);
 cache.put(2, 2);
@@ -32,9 +32,10 @@ cache.put(4, 4);    // evicts key 1
 cache.get(1);       // returns -1 (not found)
 cache.get(3);       // returns 3
 cache.get(4);       // returns 4
+
 */
 
-
+/*
 import java.util.LinkedHashMap;
 class LRUCache 
 {
@@ -99,3 +100,105 @@ class LRUCache
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+//USING DLINKED LIST
+
+ class LRUCache
+ {
+     class DLinkedNode
+     {
+         int key;
+         int value;
+
+         DLinkedNode next;
+         DLinkedNode prev;
+     }
+
+     void addNode(DLinkedNode node)
+     {
+         node.prev = head;
+         node.next = head.next;
+
+         head.next.prev = node;
+         head.next = node;
+     }
+
+     void removeNode(DLinkedNode node)
+     {
+         DLinkedNode prev = node.prev;
+         DLinkedNode next = node.next;
+
+         next.prev = prev;
+         prev.next = next;
+     }
+
+     void moveToHead(DLinkedNode node)
+     {
+         removeNode(node);
+         addNode(Node);
+     }
+
+     void popTail()
+     {
+         DLinkedNode t = tail.prev;
+         removeNode(t);
+         return t;
+     }
+
+    HashMap<Integer, DLinkedNode> cache = new HashMap<>();
+    int size;
+    int capacity;
+    DLinkedNode head, tail;
+
+    public LRUCache(int capacity) 
+    {
+        this.size = 0;
+        this.capacity = capacity; 
+
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+
+        head.next = tail;
+        tail.prev = head;       
+    }
+
+    public int get(int key) 
+    {
+        DLinkedNode node = cache.get(key);
+        if(node == null) return -1;
+
+        moveToHead(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value)
+    {
+        DLinkedNode Node = cache.get(key);
+
+        if(Node == null)
+        {
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.value = value;
+            newNode.key = key;
+
+            cache.put(key, newNode);
+            addNode(newNode);
+            ++size;
+
+            if(size > capacity)
+            {
+                DlinkedNode tail = popTail();
+                cache.remove(tail.key);
+                --size;
+            }
+
+        }
+        else 
+        {
+            Node.value = value;
+            moveToHead(newNode);
+        }
+    }
+
+ }
