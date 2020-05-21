ac62f762f3e8dff99ee6dd806a0b460b90e15a97
Riju <59536881+riju92@users.noreply.github.com>
2020-05-21 01:25:44 -0400

    2_add two numbers

---
+++ Miscellaneous/2_Add two numbers/Solution.java
@@ -0,0 +1,60 @@
+/*
+2. Add Two Numbers
+Medium
+
+7856
+
+2013
+
+Add to List
+
+Share
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
+    {
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0)
+        {
+            int sum = carry;
+            if(l1 != null)
+            {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
+            carry = sum / 10;
+            ListNode result = new ListNode(sum%10);
+            current.next = result;
+            current = current.next;
+        }
+        return head.next;
+    }
+}
