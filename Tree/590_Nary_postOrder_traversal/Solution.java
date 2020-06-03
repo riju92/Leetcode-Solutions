b12486db9459eef4e62978abdd4961c9725225b1
riju92 <dasrishabh@gmail.com>
2020-06-03 13:04:33 -0400

    590 post order traversal

---
+++ Tree/590_Nary_postOrder_traversal/Solution.java
@@ -0,0 +1,90 @@
+/*
+Given an n-ary tree, return the postorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+ 
+
+Follow up:
+
+Recursive solution is trivial, could you do it iteratively?
+
+ 
+
+Example 1:
+
+
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [5,6,3,2,4,1]
+Example 2:
+
+
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+*/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+// RECURSIVE
+class Solution
+{
+	List<Integer> list = new ArrayList<>();
+	public List<Integer> postorder(Node root)
+	{
+		if(root == null) return list;
+
+		for(Node node : root.children)
+		{
+			postorder(node);
+		}
+
+		list.add(root.val);
+		return list;
+	}
+
+}
+
+//ITERATIVE
+class Solution
+{
+	public List<Integer> postorder(Node root)
+	{
+		List<Integer> list = new ArrayList<>();
+		if(root == null) return list;
+
+		Stack<Node> stack = new Stack<>();
+		stack.add(root);
+
+		while(!stack.isEmpty())
+		{
+			root = stack.pop();
+			list.add(root.val);
+			for(Node node : root.children)
+			{
+				stack.add(node);
+			}
+		}
+		Collections.reverse(list);
+		return list;
+	}
+}
+
