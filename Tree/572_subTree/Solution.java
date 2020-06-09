8ab67ce63b4ce2e541d11031c3efde91dff8ba36
riju92 <dasrishabh@gmail.com>
2020-06-09 00:09:30 -0400

    572_subTree

---
+++ Tree/572_subTree/Solution.java
@@ -0,0 +1,68 @@
+/*
+ * Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+
+Example 1:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+   4
+  / \
+ 1   2
+Return true, because t has the same structure and node values with a subtree of s.
+
+
+Example 2:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+Given tree t:
+   4
+  / \
+ 1   2
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+class Solution
+{
+	public boolean isSubtree(TreeNode s, TreeNode t)
+	{
+		if(s == null) return false;
+		if(isSame(s, t)) return true;
+
+		return isSubtree(s.right, t) || isSubtree(s.left, t);
+	}	
+
+	public boolean isSame(TreeNode s, TreeNode t)
+	{
+		if(s == null || t == null) return s == t;
+		if(s.val != t.val) return false;
+		return isSame(s.left, t.left) && isSame(s.right, t.right);
+	}
+}
