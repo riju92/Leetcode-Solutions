bce1622c82f83f8c7ef00187c1bf373079de030e
riju92 <dasrishabh@gmail.com>
2020-07-30 19:28:00 -0400

    215_kthLArgest 347_KFrequent

---
+++ Divide and Conquer/215_KthLargestElement/Solution.java
@@ -0,0 +1,33 @@
+/*
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+Note:
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+*/
+
+class Solution
+{
+    public int findKthLargest(int[] nums, int k)
+    {
+        int result = 0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i : nums)
+        {
+            minHeap.add(i);
+
+            if(minHeap.size() > k)
+                minHeap.remove();
+        }
+
+        result = minHeap.poll();
+        return result;
+    }
+}
